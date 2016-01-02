82fd701315bb900db2f4565cc10bfc57124e199d
lahiru <lahirupathiranalr@gmail.com>
2016-01-02 16:52:11 +0530

    updated.

--- payforme.php
+++ payforme.php
@@ -5,6 +5,7 @@
  * Date: 28/09/15
  * Time: 22:27
  */
+require_once 'core/init.php';
 ?>
 
 <!DOCTYPE html>
@@ -26,6 +27,16 @@
     ?>
     <br>
     <div id="HomeForm" class="jumbotron col-sm-6 col-sm-offset-1">
+<!--      pay for other person check  -->
+        <?php
+        if(!isset($_SESSION['p4o']) || $_SESSION['p4o']==0){
+            echo "<div><a href='payForOther.php' onclick=\"return confirm('Are you sure?')\">Pay For Other >></a></div>";
+        }
+        if(isset($_SESSION['p4o']) && $_SESSION['p4o']==1){
+            echo "<div class='alert alert-info'>You are paying for ".$_SESSION['payeeName']."<button class=\"btn btn-default btn-xs\" style=\"float:right\"><a href='payForOtherRemove.php' title='Click here to remove other person.'>Remove</a></button></div> ";
+        }
+        ?>
+<!--        /payfor other person check-->
         <h3><strong>Select your Payment</strong></h3>
         <div>
             <a  href="p_UCSCregistration.php">Register to UCSC</a>
