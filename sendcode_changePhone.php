dd130c98c01f049afd1e8eb9c94c88ca9eb3385a
lahiru <lahirupathiranalr@gmail.com>
2016-01-02 10:50:36 +0530

    update completed

--- sendcode_changePhone.php
+++ sendcode_changePhone.php
@@ -20,8 +20,9 @@ $messageArray = $file->read_newLine('Files/messages');
 if(!isset($_POST['sending2'])){
     $phNumber = $_SESSION['new_number'];
     $to ='94'.substr($phNumber,1,9);
-    $notification->send($detailArray[0], $to, $messageArray[2] . " " . $randomValue, $detailArray[1]);
+    $notification->send($detailArray[0], $to, $messageArray[1] . " " . $randomValue, $detailArray[1]);
 }
+$_SESSION['s2']=1;//to check verification sended or not
 Redirect::to('confirmPNum.php');
 
 ?>
