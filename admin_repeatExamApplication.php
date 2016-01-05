175add542021d6db8bfd1843e205e7e9e5456413
lahiru <lahirupathiranalr@gmail.com>
2016-01-05 19:49:03 +0530

    updated by anjana

--- admin_repeatExamApplication.php
+++ admin_repeatExamApplication.php
@@ -47,7 +47,7 @@ if($_SESSION['student']){
                         <?php
                         $appDet = DB::getInstance()->get('repeat_exam',array('adminStatus','=',0));
                         if(!$appDet->count()){
-                        echo 'No transactions';
+                        echo '<div class="alert alert-info">No transactions</div>';
                         }else{
                         ?>
                         <thead>
@@ -75,7 +75,8 @@ if($_SESSION['student']){
                             echo "<td>".$t->subjectName."</td>";
                             echo "<td>".$t->AssignmentComplete."</td>";
     //                        echo "<td><button><a>".$t->adminStatus."</a></button></td>";
-                            echo "<td><a href='admin_repeatExamStatusUpdater.php?id=".$id."&accept=true' onclick='return confirm(\"You are accepting this application. Are you sure?\");'><button class='btn btn-primary'>Accept</button></a> <a href='admin_repeatExamStatusUpdater.php?id=".$id."&reject=true' onclick='return confirm(\"You are rejecting this application. Are you sure?\");'><button class='btn btn-danger'>Reject</button></a></td>";
+//                            echo "<td><a href='admin_repeatExamStatusUpdater.php?id=".$id."&accept=true' onclick='return confirm(\"You are accepting this application. Are you sure?\");'><button class='btn btn-primary'>Accept</button></a> <a href='admin_repeatExamStatusUpdater.php?id=".$id."&reject=true' onclick='return confirm(\"You are rejecting this application. Are you sure?\");'><button class='btn btn-danger'>Reject</button></a></td>";
+                            echo "<td><a href='admin_repeatExamStatusUpdater.php?subCode=$t->subjectCode&subName=$t->subjectName&id=".$id."&accept=true' onclick='return confirm(\"You are accepting this application. Are you sure?\");'><button class='btn btn-primary'>Accept</button></a> <a href='admin_repeatExamStatusUpdater.php?id=".$id."&reject=true' onclick='return confirm(\"You are rejecting this application. Are you sure?\");'><button class='btn btn-danger'>Reject</button></a></td>";
                             echo "</tr>";
                         }
                         }?>
--- admin_repeatExamStatusUpdater.php
+++ admin_repeatExamStatusUpdater.php
@@ -8,15 +8,30 @@
 require_once 'core/init.php';
 
 $id = $_GET['id'];
+$subCode=$_GET['subCode'];
+$subName=$_GET['subName'];
+$subject="Regarding to repeat Application";
+
+$mailObject =new Mail();
+$acceptMSG= "Dear Student, Your Repeat Examination application on"." ". $subCode." ". $subName. " " . " has been Accepted";
+$ignoreMSG= "Dear Student, Your Repeat Examination application on"." ". $subCode." ". $subName. " " . " has been ignored. Please meet your course coordinator";
+
+
+$user = new User();
+$mail= $user->data()->email;
+
+$id = $_GET['id'];
 if(isset($_GET['accept'])){
     if($_GET['accept']==true){
         $tdb1 = DB::getInstance()->update('repeat_exam',$id,array('adminStatus' => 1));
+        $mailObject ->sendMail($mail,$subject,$acceptMSG);
         Redirect::to('admin_repeatExamApplication.php');
     }
 }
 if(isset($_GET['reject'])){
     if($_GET['reject']==true){
         $tdb1 = DB::getInstance()->update('repeat_exam',$id,array('adminStatus' => 2));
+        $mailObject ->sendMail($mail,$subject,$ignoreMSG);
         Redirect::to('admin_repeatExamApplication.php');
     }
 }
