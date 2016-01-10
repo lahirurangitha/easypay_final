5bff6059667e17adef7c1814970c62c07f16792e
lahiru <lahirupathiranalr@gmail.com>
2016-01-10 23:43:29 +0530

    update 1-10-2016

--- admin_enableDisableUserStatusUpdater.php
+++ admin_enableDisableUserStatusUpdater.php
@@ -13,14 +13,19 @@ if(!$user->hasPermission('admin')){Redirect::to('index.php');}
 
 $username = $_GET['username'];
 $currStatus = $_GET['active'];
-if($currStatus==1){
-    $deactivate = DB::getInstance()->query('UPDATE users SET active = 0 WHERE username = ?',array($username));
-    if($deactivate->count()>0){
-        echo "<script>alert('Deactivated');window.location.href='admin_enableDisableUser.php'</script>";
+if($user->data()->username!=$username){
+    if($currStatus==1){
+        $deactivate = DB::getInstance()->query('UPDATE users SET active = 0 WHERE username = ?',array($username));
+        if($deactivate->count()>0){
+            echo "<script>alert('Deactivated');window.location.href='admin_enableDisableUser.php'</script>";
+        }
+    }elseif($currStatus==0){
+        $activate = DB::getInstance()->query('UPDATE users SET active = 1 WHERE username = ?',array($username));
+        if($activate->count()>0){
+            echo "<script>alert('Activated');window.location.href='admin_enableDisableUser.php'</script>";
+        }
     }
-}elseif($currStatus==0){
-    $activate = DB::getInstance()->query('UPDATE users SET active = 1 WHERE username = ?',array($username));
-    if($activate->count()>0){
-        echo "<script>alert('Activated');window.location.href='admin_enableDisableUser.php'</script>";
-    }
+}else{
+    echo "<script>alert('You cannot deactivate your own account.');window.location.href='admin_enableDisableUser.php'</script>";
 }
+
