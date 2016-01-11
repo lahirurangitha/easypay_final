d63bded64089e64b7a8a55cd3a6e9b7ed2880295
lahiru <lahirupathiranalr@gmail.com>
2016-01-11 10:14:56 +0530

    email sending updated

--- coord_repeatExamStatusUpdater.php
+++ coord_repeatExamStatusUpdater.php
@@ -6,14 +6,16 @@
  * Time: 9:21 PM
  */
 require_once 'core/init.php';
+require_once 'PHPMailer/PHPMailerAutoload.php';
+require_once 'PHPMailer/class.phpmailer.php';
+require_once 'PHPMailer/class.smtp.php';
 $id = $_GET['id'];
 $subCode=$_GET['subCode'];
 $subName=$_GET['subName'];
 $username=$_GET['username'];
-$subject="Regarding to repeat Application";
-$mailObject =new Mail();
-$acceptMSG= "Dear Student, Your Repeat Examination application on"." ". $subCode." ". $subName. " " . " has been Accepted";
-$ignoreMSG= "Dear Student, Your Repeat Examination application on"." ". $subCode." ". $subName. " " . " has been Rejected. Please meet your course coordinator";
+$subjectTitle="Regarding to repeat Application";
+$acceptMSG= "Dear Student, Your Repeat Examination application on'"." ". $subCode." ". $subName. " " . "' has been Accepted";
+$ignoreMSG= "Dear Student, Your Repeat Examination application on'"." ". $subCode." ". $subName. " " . "'has been Rejected. Please meet your course coordinator";
 $catchMail=DB::getInstance()->get('users',array('username','=',$username));
 $res=$catchMail->results()[0];
 $uID = $res->id;
@@ -23,7 +25,61 @@ if(isset($_GET['accept'])){
     if($_GET['accept']==true){
         $tdb1 = DB::getInstance()->update('repeat_exam',$id,array('adminStatus' => 1));
         $tdb2 = DB::getInstance()->insert('repeatExam_notification',array('uID' => $uID,'topic' => 'Application Accepted','description' => $acceptMSG));
-        $mailObject ->sendMail($mail,$subject,$acceptMSG);
+//        $mailObject ->sendMail($mail,$subject,$acceptMSG);
+        //mail
+        /* CONFIGURATION */
+        $crendentials = array(
+            'email'     => 'easypayucsc2@gmail.com',    //Your GMail adress
+            'password'  => 'easypayucsc@123'               //Your GMail password
+        );
+
+        /* SPECIFIC TO GMAIL SMTP */
+        $smtp = array(
+
+            'host' => 'smtp.gmail.com',
+            'port' => 587,
+            'username' => $crendentials['email'],
+            'password' => $crendentials['password'],
+            'secure' => 'tls' //SSL or TLS
+
+        );
+
+        /* TO, SUBJECT, CONTENT */
+        $to         = $mail; //The 'To' field
+        $subject    = $subjectTitle;
+
+
+
+        $mailer = new PHPMailer();
+
+//SMTP Configuration
+        $mailer->isSMTP();
+        $mailer->SMTPAuth   = true; //We need to authenticate
+        $mailer->Host       = $smtp['host'];
+        $mailer->Port       = $smtp['port'];
+        $mailer->Username   = $smtp['username'];
+        $mailer->Password   = $smtp['password'];
+        $mailer->SMTPSecure = $smtp['secure'];
+
+//Now, send mail :
+//From - To :
+        $mailer->From       = $crendentials['email'];
+        $mailer->FromName   = 'Easy Pay'; //Optional
+        $mailer->addAddress($to);  // Add a recipient
+
+//Subject - Body :
+        $mailer->Subject        = $subject;
+        $mailer->Body           = $acceptMSG;
+        $mailer->isHTML(true); //Mail body contains HTML tags
+
+//Check if mail is sent :
+        if(!$mailer->send()) {
+            echo 'Error sending mail : ' . $mailer->ErrorInfo;
+        } else {
+            echo 'Message sent !';
+        }
+
+        //
         Redirect::to('coord_repeatExamApplication.php');
     }
 }
@@ -31,7 +87,60 @@ if(isset($_GET['reject'])){
     if($_GET['reject']==true){
         $tdb1 = DB::getInstance()->update('repeat_exam',$id,array('adminStatus' => 2));
         $tdb2 = DB::getInstance()->insert('repeatExam_notification',array('uID' => $uID,'topic' => 'Application Rejected','description' => $ignoreMSG));
-        $mailObject ->sendMail($mail,$subject,$ignoreMSG);
+//        $mailObject ->sendMail($mail,$subject,$ignoreMSG);
+        //mail
+        /* CONFIGURATION */
+        $crendentials = array(
+            'email'     => 'easypayucsc2@gmail.com',    //Your GMail adress
+            'password'  => 'easypayucsc@123'               //Your GMail password
+        );
+
+        /* SPECIFIC TO GMAIL SMTP */
+        $smtp = array(
+
+            'host' => 'smtp.gmail.com',
+            'port' => 587,
+            'username' => $crendentials['email'],
+            'password' => $crendentials['password'],
+            'secure' => 'tls' //SSL or TLS
+
+        );
+
+        /* TO, SUBJECT, CONTENT */
+        $to         = $mail; //The 'To' field
+        $subject    = $subjectTitle;
+
+
+
+        $mailer = new PHPMailer();
+
+//SMTP Configuration
+        $mailer->isSMTP();
+        $mailer->SMTPAuth   = true; //We need to authenticate
+        $mailer->Host       = $smtp['host'];
+        $mailer->Port       = $smtp['port'];
+        $mailer->Username   = $smtp['username'];
+        $mailer->Password   = $smtp['password'];
+        $mailer->SMTPSecure = $smtp['secure'];
+
+//Now, send mail :
+//From - To :
+        $mailer->From       = $crendentials['email'];
+        $mailer->FromName   = 'Easy Pay'; //Optional
+        $mailer->addAddress($to);  // Add a recipient
+
+//Subject - Body :
+        $mailer->Subject        = $subject;
+        $mailer->Body           = $ignoreMSG;
+        $mailer->isHTML(true); //Mail body contains HTML tags
+
+//Check if mail is sent :
+        if(!$mailer->send()) {
+            echo 'Error sending mail : ' . $mailer->ErrorInfo;
+        } else {
+            echo 'Message sent !';
+        }
+        //
         Redirect::to('coord_repeatExamApplication.php');
     }
 }
